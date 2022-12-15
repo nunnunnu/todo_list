f699475b7237dbffeb99a0f838c95f2bc72698a8
pjh <bis04199@gmail.com>
2022-12-15 21:47:21 +0900

    221215 N

--- src/main/java/com/jh_project/todo/member/repository/memberRepository.java
+++ src/main/java/com/jh_project/todo/member/repository/memberRepository.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.jh_project.todo.member.entity.MemberInfoEntity;
 @Repository
-public interface MemberRepository extends JpaRepository<MemberInfoEntity, Long> {
+public interface MemberRepository extends JpaRepository<MemberInfoEntity, Long>{
     public Integer countByEmail(String email);
     public MemberInfoEntity findByEmailAndPwd(String email, String pwd);
-    
 }
