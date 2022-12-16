8b70a4804e2992f8d23e9743c65e80704110e117
jh <bis04199@gmail.com>
2022-12-16 09:28:33 +0900

    221216 M

--- src/main/java/com/jh_project/todo/member/repository/memberRepository.java
+++ src/main/java/com/jh_project/todo/member/repository/memberRepository.java
@@ -8,5 +8,6 @@ import com.jh_project.todo.member.entity.MemberInfoEntity;
 public interface MemberRepository extends JpaRepository<MemberInfoEntity, Long> {
     public Integer countByEmail(String email);
     public MemberInfoEntity findByEmailAndPwd(String email, String pwd);
+    //sss
     
 }
